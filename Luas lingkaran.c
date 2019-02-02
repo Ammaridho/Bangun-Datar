745fd59a0e3d7b7223709a421b07360137f7fca0
andisetiawn <andiep166@gmail.com>
2019-02-02 20:29:38 +0700

    tambah luas lingkaran

--- Luas lingkaran.c
+++ Luas lingkaran.c
@@ -1,4 +1,4 @@
-//Program luaslingkaran;
+//Program luaspersegi;
 //{program ini digunakan untuk menghitung luas}
 
 //Deklarasi
@@ -21,10 +21,32 @@ void lingkaran() {
 	//Output
 	printf("Luas Lingkaran:%f",luas_lingkaran);
 }
+
+void persegi(){
+	
+	float s1,s2;
+	float luas_persegi;
+	
+	//input
+	printf("Masukan Sisi 1 : ");
+	scanf("%f",&s1);
+	
+	printf("Masukan Sisi 2 : ");
+	scanf("%f",&s2);
+	
+	//proses
+	luas_persegi = s1*s2;
+	
+	//Output
+	printf("Luas Persegi : %f",luas_persegi);
+	
+}
+
 int menu(){
 	int pilihan;
 	printf("0. exit\n");
 	printf("1. luas lingkaran\n");
+	printf("4. luas persegi\n");
 	printf("masukan pilihan : \n");
 	scanf("%d", &pilihan);
 	switch(pilihan){
@@ -33,6 +55,8 @@ int menu(){
 		case 1 :
 			lingkaran();
 			break;
+		case 4 :
+			persegi();
 		default :
 			printf("invailid");
 
