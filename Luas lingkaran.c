f7953530f0fa823377697b2fedfb85e17494cbd2
herbertabdillah <abdillah.herbert@gmail.com>
2019-02-02 20:12:31 +0700

    rapihin

--- Luas lingkaran.c
+++ Luas lingkaran.c
@@ -3,20 +3,48 @@
 
 //Deklarasi
 #include<stdio.h>
-float r;
-float luas_lingkaran;
-float Phi = 3.14;
+
+const float PHI = 3.14;
 
 //Algoritma
-int main (){
+void lingkaran() {
+	float r;
+	float luas_lingkaran;
+	
 	//input
 	printf("Masukan Jari jari: ");
 	scanf("%f",&r);
 	
 	//proses
-	luas_lingkaran = Phi * r * r;
+	luas_lingkaran = PHI * r * r;
 	
 	//Output
 	printf("Luas Lingkaran:%f",luas_lingkaran);
-//	return 0;
+}
+int menu(){
+	int pilihan;
+	printf("0. exit\n");
+	printf("1. luas lingkaran\n");
+	printf("masukan pilihan : \n");
+	scanf("%d", &pilihan);
+	switch(pilihan){
+		case 0 :
+			return 0;
+		case 1 :
+			lingkaran();
+			break;
+		default :
+			printf("invailid");
+
+	}
+}
+int main (){
+	
+	while(1) {
+		if(menu() == 0) {
+			break;
+		}
+	}
+	
+	return 0;
 }
