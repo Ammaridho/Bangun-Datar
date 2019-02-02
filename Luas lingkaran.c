bf44f2e2df0230670e1049590b2b3b841f515e55
Ammaridho <siregarridho28@gmail.com>
2019-02-02 20:51:04 +0700

    luas keliling segitga

--- Luas lingkaran.c
+++ Luas lingkaran.c
@@ -21,10 +21,34 @@ void lingkaran() {
 	//Output
 	printf("Luas Lingkaran:%f",luas_lingkaran);
 }
+
+void segitiga(){
+	float alas;
+	float tinggi;
+	float luas_segitiga;
+	float keliling_segitiga;
+	
+	//input
+	printf("Masukkan alas: ");
+	scanf("%f",&alas);
+	
+	printf("Masukkan tinggi: ");
+	scanf("%f",&tinggi);
+	
+	//proses
+	luas_segitiga = alas * tinggi / 2;
+	keliling_segitiga = alas * 3;
+	
+	//output
+	printf("Luas Segitiga:%f",luas_segitiga);
+	printf("Keliling Segitiga:%f",keliling_segitiga);
+}
+
 int menu(){
 	int pilihan;
 	printf("0. exit\n");
 	printf("1. luas lingkaran\n");
+	printf("2. luas segitiga\n");
 	printf("masukan pilihan : \n");
 	scanf("%d", &pilihan);
 	switch(pilihan){
@@ -33,11 +57,15 @@ int menu(){
 		case 1 :
 			lingkaran();
 			break;
+		case 2 :
+			segitiga();
+			break;
 		default :
 			printf("invailid");
 
 	}
 }
+
 int main (){
 	
 	while(1) {
