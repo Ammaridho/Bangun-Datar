9649517fcd33ea126fd2b11d756ebd6f0efe1b11
mdanirfan <mdanirfan93@gmail.com>
2019-02-02 20:52:50 +0700

    luas keliling segitiga

--- Luas lingkaran.c
+++ Luas lingkaran.c
@@ -7,6 +7,27 @@
 const float PHI = 3.14;
 
 //Algoritma
+void persegipanjang(){
+	float panjang;
+	float lebar;
+	float luas_pp;
+	float keliling_pp;
+	
+	//input
+	printf("Masukan Panjang : ");
+	scanf("%f",&panjang);
+	printf("Masukan Lebar : ");
+	scanf("%f",&lebar);
+	
+	//proses
+	luas_pp = panjang * lebar;
+	keliling_pp = 2 * (panjang + lebar);
+	
+	//output
+	printf("Luas Persegi Panjang:%f\n",luas_pp);
+	printf("Keliling Persegi Panjang:%f\n",keliling_pp);
+
+}
 void lingkaran() {
 	float r;
 	float luas_lingkaran;
@@ -25,6 +46,7 @@ int menu(){
 	int pilihan;
 	printf("0. exit\n");
 	printf("1. luas lingkaran\n");
+	printf("3. Persegi Panjang\n");
 	printf("masukan pilihan : \n");
 	scanf("%d", &pilihan);
 	switch(pilihan){
@@ -32,6 +54,9 @@ int menu(){
 			return 0;
 		case 1 :
 			lingkaran();
+			break;
+		case 3 :
+			persegipanjang();
 			break;
 		default :
 			printf("invailid");
